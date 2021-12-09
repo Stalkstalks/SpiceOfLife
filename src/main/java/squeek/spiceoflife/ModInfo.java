1003d1045883ee0f306e84d3447acda7bbd489d5
Johann Bernhardt <johann.bernhardt@tum.de>
2021-12-09 15:28:00 +0100

    Move sources and resources

--- src/main/java/squeek/spiceoflife/ModInfo.java
+++ src/main/java/squeek/spiceoflife/ModInfo.java
@@ -2,6 +2,6 @@ package squeek.spiceoflife;
 
 public final class ModInfo {
     public static final String MODID = "SpiceOfLife";
-    public static final String VERSION = "${version}";
+    public static final String VERSION = "GRADLETOKEN_VERSION";
     public static final String NETCHANNEL = ModInfo.MODID;
 }
