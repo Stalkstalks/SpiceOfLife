f4b894811caf8025bb3183641c36805944ffa481
bombcar <github@bombcar.com>
2023-11-06 13:14:31 -0600

    Update buildscript and spotless

--- src/main/java/squeek/spiceoflife/gui/GuiScreenFoodJournal.java
+++ src/main/java/squeek/spiceoflife/gui/GuiScreenFoodJournal.java
@@ -1,9 +1,13 @@
 package squeek.spiceoflife.gui;
 
-import java.awt.*;
+import java.awt.Color;
 import java.text.DecimalFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.Gui;
